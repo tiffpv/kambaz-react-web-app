b057491e0c42ec7d65c108125bdf9d11534078db
Tiffany Pacas Villanueva <tiffanypacas97@gmail.com>
2025-01-22 21:06:18 -0500

    worked on a1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -173,7 +173,7 @@ export default function Lab1() {
                 <br />
                 Loading a local image:
                 <br />
-                <img id="wd-teslabot" src="public/teslabot.jpg" height="200px"
+                <img id="wd-teslabot" src="public/images/teslabot.jpg" height="200px"
                 />
             </div>
             <div id="wd-forms">
