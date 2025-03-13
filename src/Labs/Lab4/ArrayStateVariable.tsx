3006aafd96c4df96dd6ef8a376003188c9653100
Tiffany Pacas Villanueva <tiffanypacas97@gmail.com>
2025-03-13 07:26:39 -0400

    minor changes

--- src/Labs/Lab4/ArrayStateVariable.tsx
+++ src/Labs/Lab4/ArrayStateVariable.tsx
@@ -4,20 +4,20 @@ export default function ArrayStateVariable() {
     const [array, setArray] = useState([1, 2, 3, 4, 5]);
     const addElement = () => {
         setArray([...array, Math.floor(Math.random() * 100)]);
- };
+    };
     const deleteElement = (index: number) => {
-        setArray(array.filter((item, i) => i !== index));
- };
+        setArray(array.filter((_, i) => i !== index));
+    };
     return(
         <div id="wd-array-state-variables">
             <h2>Array State Variable</h2>
             <button onClick={addElement}
-                className="btn btn-success">Add Element</button>
+                className="btn btn-success">Add Element</button><hr/>
             <ul>
                 {array.map((item, index) => (
                 <div key={index}> {item} 
                     <button onClick={() => deleteElement(index)} 
-                        className="btn btn-danger mx-2">Delete</button>
+                        className="btn btn-danger mx-2">Delete</button><hr/>
                 </div>))}
             </ul>
             <hr/>
