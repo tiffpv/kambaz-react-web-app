29421fc0d884f8dcf9f20a505052146dfbfc3462
Tiffany Pacas Villanueva <tiffanypacas97@gmail.com>
2025-03-31 21:33:16 -0400

    made changes

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -58,150 +58,157 @@ export default function AssignmentsEditor() {
   if (!isFaculty) {
     return (
       <Container className="p-4">
-        <Form.Group className="mb-3">
-          <Form.Label>
-            <strong>Assignment Name</strong>
-          </Form.Label>
-          <Form.Control
-            type="text"
-            value={assignment.title}
-            onChange={(e) => handleUpdate("title", e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>
-            <strong>Description</strong>
-          </Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={6}
-            value={assignment.description || ""}
-            onChange={(e) => handleUpdate("description", e.target.value)}
-          />
-        </Form.Group>
-
-        <Row className="mb-3">
-          <Col md={3} className="text-end align-top">
-            Points
-          </Col>
-          <Col md={9}>
-            <Form.Control
-              type="number"
-              value={assignment.points}
-              onChange={(e) => handleUpdate("points", Number(e.target.value))}
-            />
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col md={3} className="text-end align-top">
-            Assignment Group
-          </Col>
-          <Col md={9}>
-            <Form.Select defaultValue="ASSIGNMENTS">
-              <option value="Percentage">Assignments</option>
-            </Form.Select>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col md={3} className="text-end align-top">
-            Display Grade as
-          </Col>
-          <Col md={9}>
-            <Form.Select defaultValue="Percentage">
-              <option value="Percentage">Percentage</option>
-              <option value="Complete/Incomplete">Complete/Incomplete</option>
-              <option value="Letter Grade">Letter Grade</option>
-              <option value="GPA Scale">GPA Scale</option>
-            </Form.Select>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col md={3} className="text-end align-top">
-            Submission Type
-          </Col>
-          <Col md={9}>
-            <Form.Select defaultValue="Online">
-              <option value="Online">Online</option>
-            </Form.Select>
-            <br />
-            <strong>Online Entry Options</strong>
-            <br />
-            <Form.Check type="checkbox" label="Text Entry" />
-            <Form.Check type="checkbox" label="Website URL" />
-            <Form.Check type="checkbox" label="Media Recordings" />
-            <Form.Check type="checkbox" label="Student Annotation" />
-            <Form.Check type="checkbox" label="File Uploads" />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={2} className="text-end align-top">
-            Assign
-          </Col>
-          <Col md={10}>
-            <Card className="p-3 rounded">
-              <Row className="mb-3">
-                <Col md={12}>
-                  <Form.Label>
-                    <strong>Assign to</strong>
-                  </Form.Label>
-                  <Form.Control type="text" defaultValue="Everyone" />
-                </Col>
-              </Row>
-              <Row className="mb-3">
-                <Col md={12}>
-                  <Form.Label>
-                    <strong>Due</strong>
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={assignment.due}
-                    onChange={(e) => handleUpdate("due", e.target.value)}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                  <Form.Label>
-                    <strong>Available from</strong>
-                  </Form.Label>
-                  <Form.Control
-                    type="date"
-                    value={assignment.availableFrom}
-                    onChange={(e) =>
-                      handleUpdate("availableFrom", e.target.value)
-                    }
-                  />
-                </Col>
-                <Col md={6}>
-                  <Form.Label>
-                    <strong>Until</strong>
-                  </Form.Label>
-                  <Form.Control
-                    type="date"
-                    value={assignment.availableUntil}
-                    onChange={(e) =>
-                      handleUpdate("availableUntil", e.target.value)
-                    }
-                  />
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-        </Row>
-        <div className="mt-4 text-end">
-          <hr />
-          <Button variant="light" className="me-2" onClick={handleCancel}>
-            Cancel
-          </Button>
-          <Button variant="danger" onClick={handleSave}>
-            Save
-          </Button>
-        </div>
+        <h3>Denied!</h3>
+        <p>Only FACULTY can add or edit assignments</p>
       </Container>
     );
   }
+
+  return (
+    <Container className="p-4">
+      <Form.Group className="mb-3">
+        <Form.Label>
+          <strong>Assignment Name</strong>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          value={assignment.title}
+          onChange={(e) => handleUpdate("title", e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>
+          <strong>Description</strong>
+        </Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          value={assignment.description || ""}
+          onChange={(e) => handleUpdate("description", e.target.value)}
+        />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Col md={3} className="text-end align-top">
+          Points
+        </Col>
+        <Col md={9}>
+          <Form.Control
+            type="number"
+            value={assignment.points}
+            onChange={(e) => handleUpdate("points", Number(e.target.value))}
+          />
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col md={3} className="text-end align-top">
+          Assignment Group
+        </Col>
+        <Col md={9}>
+          <Form.Select defaultValue="ASSIGNMENTS">
+            <option value="Percentage">Assignments</option>
+          </Form.Select>
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col md={3} className="text-end align-top">
+          Display Grade as
+        </Col>
+        <Col md={9}>
+          <Form.Select defaultValue="Percentage">
+            <option value="Percentage">Percentage</option>
+            <option value="Complete/Incomplete">Complete/Incomplete</option>
+            <option value="Letter Grade">Letter Grade</option>
+            <option value="GPA Scale">GPA Scale</option>
+          </Form.Select>
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col md={3} className="text-end align-top">
+          Submission Type
+        </Col>
+        <Col md={9}>
+          <Form.Select defaultValue="Online">
+            <option value="Online">Online</option>
+          </Form.Select>
+          <br />
+          <strong>Online Entry Options</strong>
+          <br />
+          <Form.Check type="checkbox" label="Text Entry" />
+          <Form.Check type="checkbox" label="Website URL" />
+          <Form.Check type="checkbox" label="Media Recordings" />
+          <Form.Check type="checkbox" label="Student Annotation" />
+          <Form.Check type="checkbox" label="File Uploads" />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={2} className="text-end align-top">
+          Assign
+        </Col>
+        <Col md={10}>
+          <Card className="p-3 rounded">
+            <Row className="mb-3">
+              <Col md={12}>
+                <Form.Label>
+                  <strong>Assign to</strong>
+                </Form.Label>
+                <Form.Control type="text" defaultValue="Everyone" />
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col md={12}>
+                <Form.Label>
+                  <strong>Due</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={assignment.due}
+                  onChange={(e) => handleUpdate("due", e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Form.Label>
+                  <strong>Available from</strong>
+                </Form.Label>
+                <Form.Control
+                  type="date"
+                  value={assignment.availableFrom}
+                  onChange={(e) =>
+                    handleUpdate("availableFrom", e.target.value)
+                  }
+                />
+              </Col>
+              <Col md={6}>
+                <Form.Label>
+                  <strong>Until</strong>
+                </Form.Label>
+                <Form.Control
+                  type="date"
+                  value={assignment.availableUntil}
+                  onChange={(e) =>
+                    handleUpdate("availableUntil", e.target.value)
+                  }
+                />
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+      <div className="mt-4 text-end">
+        <hr />
+        <Button variant="light" className="me-2" onClick={handleCancel}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleSave}>
+          Save
+        </Button>
+      </div>
+    </Container>
+  );
 }
